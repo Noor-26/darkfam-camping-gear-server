5bdded190436ccc781521c8a014f455cd2eb7787
Noor-26 <nk26982698@gmail.com>
2022-04-29 15:31:45 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tq1da.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const run = async () => {
+    try{
+      await client.connect();
+      console.log("database connected")
+    }  
+    finally{
+      
+    }
+
+}
+run()
+
+
+
 app.get('/',(req,res) => {
     res.send('Success the surver is running')
 })
